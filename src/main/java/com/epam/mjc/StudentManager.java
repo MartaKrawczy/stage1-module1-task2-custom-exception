f6484695ecf7104e7ee215c88cb057c9418b844a
MartaKrawczy <martaakrawczykowska@gmail.com>
2022-12-14 16:06:10 +0100

    add my own exception

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,13 +1,21 @@
 package com.epam.mjc;
 
 
+import java.util.Arrays;
+
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentNotFoundException {
+
+    if (Student.getValueOf(studentID) == null){
+      throw new StudentNotFoundException("Could not find student with ID " + studentID);
+   }else {
+      return Student.getValueOf(studentID);
+    }
   }
+
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
@@ -16,6 +24,5 @@ public class StudentManager {
       Student student = manager.find(IDs[i]);
       System.out.println("Student name " + student.getName());
     }
-
   }
 }
